360fd4e4659f18c20c6365ceb005db3bf8a77d92
Philipp Bönsch <philipp.boensch@softgames.de>
2020-07-20 16:16:10 +0200

    adjusted way of rendering rooms

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import * as PIXI from "pixi.js";
 
 const app: PIXI.Application = new PIXI.Application({width: 600, height: 400, antialias: false, backgroundColor: 0xb0b0b0, clearBeforeRender: true});
 document.body.appendChild(app.view);
-const TILE_SIZE: number = 2;
+
+const TILE_SIZE: number = 4;
 const ROOM_MIN_DIM: number = 16;
 const ROOM_MAX_DIM: number = 64;
-const ROOM_SPAWN_RADIUS: number = 128;
+const ROOM_SPAWN_RADIUS: number = 92;
 const ROOM_COUNT: number = 32;
 
 class Vector2 {
@@ -15,7 +16,65 @@ class Vector2 {
         this.x = x;
         this.y = y;
     }
+
+    add(o: Vector2) {
+        return new Vector2(this.x + o.x, this.y + o.y);
+    }
+
+    sub(o: Vector2) {
+        return new Vector2(this.x - o.x, this.y - o.y);
+    }
+
+    distSq(o: Vector2) {
+        return Math.pow(o.x - this.x, 2) + Math.pow(o.y - this.y, 2);
+    }
+
+    dist(o: Vector2) {
+        return Math.sqrt(this.distSq(o));
+    }
 }
+
+class Room {
+    width: number;
+    height: number;
+    position: Vector2;
+    graphics: PIXI.Graphics;
+    constructor(position: Vector2, width: number, height: number) {
+        this.width = width;
+        this.height = height;
+        this.position = position;
+        this.graphics = new PIXI.Graphics();
+        app.stage.addChild(this.graphics);
+        this.draw();
+    }
+
+    draw() {
+        this.graphics.clear();
+        this.graphics.lineStyle(1, 0xffffff);
+        this.graphics.beginFill(0x0f0f0f);
+        this.graphics.drawRect(this.position.x, this.position.y, this.width, this.height);
+        this.graphics.endFill();
+    }
+
+    checkForCollision(o: Room) {
+        const l1 = this.position.add(new Vector2(-this.width / 2, this.height / 2));
+        const r1 = this.position.add(new Vector2(this.width / 2, -this.height / 2));
+        const l2 = o.position.add(new Vector2(-o.width / 2, o.height / 2));
+        const r2 = o.position.add(new Vector2(o.width / 2, -o.height / 2));
+        
+        if (l1.x >= r2.x || l2.x >= r1.x) 
+            return false; 
+      
+        // If one rectangle is above other 
+        if (l1.y <= r2.y || l2.y <= r1.y) 
+            return false; 
+      
+        return true; 
+    }
+}
+
+let rooms: Array<Room> = [];
+
 
 function roundm(n: number, m: number): number {
     return Math.floor((n + m - 1) / 2) * 2;
@@ -23,7 +82,7 @@ function roundm(n: number, m: number): number {
 
 // https://stackoverflow.com/questions/5837572/generate-a-random-point-within-a-circle-uniformly/50746409#50746409
 // even though this is well explained, didn't understand it fully
-function generatePoinsWithinCircle(center: Vector2, radius: number, n: number): Vector2[] {
+function generatePositionsWithinCircle(center: Vector2, radius: number, n: number): Vector2[] {
     const points = [];
     
     for (let i = 0; i < n; i++) {
@@ -39,25 +98,42 @@ function generatePoinsWithinCircle(center: Vector2, radius: number, n: number): 
     return points;
 }
 
-function randomDimension(min: number, max: number): number {
+function randValueWithBounds(min: number, max: number): number {
     let dim = min + Math.random() * (max - min);
     return roundm(dim, TILE_SIZE);
 }
 
-function setup() {
-    const points = generatePoinsWithinCircle(new Vector2(app.view.width / 2, app.view.height / 2), ROOM_SPAWN_RADIUS, ROOM_COUNT);
+function generateRooms(points: Vector2[]): Array<Room> {
+    const rooms: Array<Room> = [];
     points.forEach(p => {
-        let square = new PIXI.Graphics();
-        square.lineStyle(1, 0xffffff);
-        square.drawRect(p.x, p.y, randomDimension(ROOM_MIN_DIM, ROOM_MAX_DIM), randomDimension(ROOM_MIN_DIM, ROOM_MAX_DIM))
-        app.stage.addChild(square);
-
+        const room = new Room(p, randValueWithBounds(ROOM_MIN_DIM, ROOM_MAX_DIM), randValueWithBounds(ROOM_MIN_DIM, ROOM_MAX_DIM));
+        rooms.push(room);
     });
+
+    return rooms;
+}
+
+function setup() {
+    const points = generatePositionsWithinCircle(new Vector2(app.view.width / 2, app.view.height / 2), ROOM_SPAWN_RADIUS, ROOM_COUNT);
+    rooms = generateRooms(points);
     app.ticker.add(delta => gameLoop(delta));
 }
 
-function gameLoop(_delta: number) {
-    
+function gameLoop(delta: number) {
+    const sorted = rooms.sort((a: Room,b: Room): number => {
+        return a.position.distSq(b.position);
+    });
+
+    for (let i = 0; i < sorted.length; i++) {
+        for (let j = 0; j < sorted.length; j++) {
+            if (i == j) continue;
+
+            if (sorted[i].checkForCollision(sorted[j])) {
+            }
+        }
+
+        sorted[i].draw();
+    }
 }
 
 setup();
