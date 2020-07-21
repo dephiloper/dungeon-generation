39f490180a3a726663cbe5e3671dd873dcce747b
Philipp Bönsch <philipp.boensch@softgames.de>
2020-07-21 23:35:53 +0200

    main room picking added
    modified system to state based approach

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,52 @@
 import * as PIXI from "pixi.js";
 import { Vector2 } from "./vec";
 
-const app: PIXI.Application = new PIXI.Application({width: 960, height: 540, antialias: false, backgroundColor: 0xb0b0b0, clearBeforeRender: true});
+const app: PIXI.Application = new PIXI.Application({ width: 960, height: 540, antialias: false, backgroundColor: 0xb0b0b0, clearBeforeRender: true });
 document.body.appendChild(app.view);
 
 const TILE_SIZE: number = 4;
 const ROOM_MIN_DIM: number = 8;
-const ROOM_MAX_DIM: number = 64;
-const ROOM_SPAWN_RADIUS: number = 92;
-const ROOM_COUNT: number = 64;
+const ROOM_MAX_DIM: number = 48;
+const ROOM_SPAWN_RADIUS: number = 48;
+const ROOM_COUNT: number = 128;
+const MAIN_ROOM_COUNT: number = 8;
 
+enum State {
+    RoomGeneration,
+    RoomSeparation,
+    MainRoomPicking,
+    Triangulation,
+    SpanningTree,
+    HallwayGeneration
+}
 
 class Room {
+    private static ID_COUNT: number = 0;
+    id: number;
+    position: Vector2;
     width: number;
     height: number;
-    position: Vector2;
+    isCollided: boolean;
     graphics: PIXI.Graphics;
-    collided: boolean;
+    isMainRoom: boolean;
     constructor(position: Vector2, width: number, height: number) {
-        this.collided = false;
+        this.id = Room.ID_COUNT++;
+        this.position = position;
         this.width = width;
         this.height = height;
-        this.position = position;
+        this.isCollided = false;
+        this.isMainRoom = false;
         this.graphics = new PIXI.Graphics();
         this.graphics.pivot.x = width / 2;
         this.graphics.pivot.y = height / 2;
+        this.graphics.visible = false;
         app.stage.addChild(this.graphics);
     }
 
     draw() {
         this.graphics.clear();
-        this.graphics.lineStyle(1, this.collided ? 0xff0000 : 0xffffff);
-        this.graphics.beginFill(0x0f0f0f);
+        this.graphics.lineStyle(1, this.isCollided ? 0xff0000 : 0xffffff);
+        this.graphics.beginFill(this.isMainRoom ? 0x006400 : 0x0f0f0f);
         this.graphics.drawRect(this.position.x, this.position.y, this.width, this.height);
         this.graphics.endFill();
     }
@@ -41,25 +56,23 @@ class Room {
         const r1 = this.position.add(new Vector2(this.width / 2, -this.height / 2));
         const l2 = o.position.add(new Vector2(-o.width / 2, o.height / 2));
         const r2 = o.position.add(new Vector2(o.width / 2, -o.height / 2));
-        
-        if (l1.x >= r2.x || l2.x >= r1.x) 
-            return false; 
-      
-        // If one rectangle is above other 
-        if (l1.y <= r2.y || l2.y <= r1.y) 
-            return false; 
-      
-        return true; 
+
+        if (l1.x >= r2.x || l2.x >= r1.x)
+            return false;
+
+        if (l1.y <= r2.y || l2.y <= r1.y)
+            return false;
+
+        return true;
     }
 }
 
 let rooms: Array<Room> = [];
-// collision test
-// let a = new Room(new Vector2(100, 100), 40, 40);
-// let b = new Room(new Vector2(200, 200), 40, 40);
-// document.onmousemove = (event) => {
-//     b.position = new Vector2(event.clientX, event.clientY);
-// }
+let points: Array<Vector2> = [];
+let tempIndex: number = 0;
+let generationState: State = State.RoomGeneration;
+let stateChanged: boolean = true;
+let elapsedTime: number = 0.0;
 
 function roundm(n: number, m: number): number {
     return Math.floor((n + m - 1) / 2) * 2;
@@ -69,7 +82,7 @@ function roundm(n: number, m: number): number {
 // even though this is well explained, didn't understand it fully
 function generatePositionsWithinCircle(center: Vector2, radius: number, n: number): Vector2[] {
     const points = [];
-    
+
     for (let i = 0; i < n; i++) {
         const r = radius * Math.sqrt(Math.random());
         const theta = Math.random() * 2 * Math.PI;
@@ -77,9 +90,9 @@ function generatePositionsWithinCircle(center: Vector2, radius: number, n: numbe
         let y = center.y + r * Math.sin(theta);
         x = roundm(x, TILE_SIZE);
         y = roundm(y, TILE_SIZE);
-        points.push(new Vector2(x,y));
+        points.push(new Vector2(x, y));
     }
-    
+
     return points;
 }
 
@@ -88,63 +101,111 @@ function randValueWithBounds(min: number, max: number): number {
     return roundm(dim, TILE_SIZE);
 }
 
-async function generateRooms(points: Vector2[]): Promise<Array<Room>> {
+function generateRooms(points: Vector2[]): Array<Room> {
     const rooms: Array<Room> = [];
     for (const p of points) {
         const room = new Room(p, randValueWithBounds(ROOM_MIN_DIM, ROOM_MAX_DIM), randValueWithBounds(ROOM_MIN_DIM, ROOM_MAX_DIM));
-        room.draw();
-        await new Promise(resolve => setTimeout(resolve, 50));
         rooms.push(room);
-        console.log("room");
     }
 
     return rooms;
 }
 
 async function setup() {
-    const points = generatePositionsWithinCircle(new Vector2(app.view.width / 2, app.view.height / 2), ROOM_SPAWN_RADIUS, ROOM_COUNT);
-    rooms = await generateRooms(points);
     app.ticker.add(delta => gameLoop(delta));
-    // app.ticker.add(delta => collisionTest(delta));
-
 }
 
-function gameLoop(delta: number) {
-    const sorted = rooms.sort((a: Room,b: Room): number => {
-        return a.position.distSq(b.position);
-    });
-
-    for (let i = 0; i < sorted.length; i++) {
-        sorted[i].collided = false;
-        for (let j = 0; j < sorted.length; j++) {
-            if (i == j) continue;
-
-            if (sorted[i].checkForCollision(sorted[j])) {
-                sorted[i].collided = true;
-                const dir = sorted[j].position.dir_to(sorted[i].position);
-                sorted[i].position = sorted[i].position.add(dir.mul(delta*2));
-                sorted[i].position = sorted[i].position.add(new Vector2(Math.random() - 0.5, Math.random() - 0.5).mul(1.5))
-            }
+function roomGeneration(_delta: number): void {
+    if (stateChanged) {
+        if (elapsedTime >= 1000) {
+            stateChanged = false;
+            points = generatePositionsWithinCircle(new Vector2(app.view.width / 2, app.view.height / 2), ROOM_SPAWN_RADIUS, ROOM_COUNT);
+            rooms = generateRooms(points);
+        }
+    } else {
+        if (elapsedTime >= 20) {
+            rooms[tempIndex++].graphics.visible = true;
+            elapsedTime = 0.0;
         }
 
-        sorted[i].draw();
+        if (rooms.every(r => r.graphics.visible)) {
+            generationState = State.RoomSeparation;
+            stateChanged = true;
+            elapsedTime = 0.0;
+            tempIndex = 0;
+        }
     }
 }
 
-// function collisionTest(delta: number) {
-//     a.collided = false;
-//     b.collided = false;
+function roomSeparation(delta: number): void {
+    if (stateChanged) {
+        if (elapsedTime >= 500) {
+            stateChanged = false;
+        }
+    } else {
+        for (let i = 0; i < rooms.length; i++) {
+            rooms[i].isCollided = false;
+            for (let j = 0; j < rooms.length; j++) {
+                if (i == j) continue;
 
-//     if (a.checkForCollision(b)) {
-//         a.collided = true;
-//     }
+                if (rooms[i].checkForCollision(rooms[j])) {
+                    rooms[i].isCollided = true;
+                    const dir = rooms[j].position.dir_to(rooms[i].position);
+                    rooms[i].position = rooms[i].position.add(dir.mul(delta * 2));
+                    rooms[i].position = rooms[i].position.add(new Vector2(Math.random() - 0.5, Math.random() - 0.5).mul(1.5));
+                }
+            }
+        }
 
-//     if (b.checkForCollision(a)) {
-//         b.collided = true;
-//     }
+        if (rooms.every(r => !r.isCollided)) {
+            generationState = State.MainRoomPicking;
+            stateChanged = true;
+            elapsedTime = 0.0;
+            tempIndex = 0;
+        }
+    }
+}
 
-//     a.draw();
-//     b.draw();
-// }
+function mainRoomPicking(_delta: number): void {
+    if (stateChanged) {
+        if (elapsedTime >= 500) {
+            stateChanged = false;
+        }
+    } else {
+        const sorted = rooms.sort((a, b) => b.width * b.height - a.width * a.height);
+        sorted[tempIndex++].isMainRoom = true;
+        if (tempIndex == MAIN_ROOM_COUNT) {
+            generationState = State.Triangulation;
+            elapsedTime = 0.0;
+            tempIndex = 0;
+        }
+    }
+}
+
+function gameLoop(delta: number): void {
+    switch (generationState) {
+        case State.RoomGeneration:
+            roomGeneration(delta);
+            break;
+        case State.RoomSeparation:
+            roomSeparation(delta);
+            break;
+        case State.MainRoomPicking:
+            mainRoomPicking(delta);
+            break;
+        case State.Triangulation:
+            break;
+        case State.SpanningTree:
+            break;
+        case State.HallwayGeneration:
+            break;
+    }
+
+    for (let i = 0; i < rooms.length; i++) {
+        rooms[i].draw();
+    }
+
+    elapsedTime += app.ticker.elapsedMS;
+}
 
 setup();
